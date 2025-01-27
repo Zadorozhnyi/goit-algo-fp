19ebcdf3eea13a7e0107976ae4b3fd548abdc5dc
Oleg Zadorozhnyi <zovjob@gmail.com>
2025-01-27 13:39:30 +0400

    Homework task 4

---
+++ task_4.py
@@ -0,0 +1,67 @@
+import uuid
+import heapq
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+
+
+def build_heap_tree(heap):
+    if not heap:
+        return None
+    
+    nodes = [Node(val) for val in heap]
+    
+    for i in range(len(nodes)):
+        left_index = 2 * i + 1
+        right_index = 2 * i + 2
+        if left_index < len(nodes):
+            nodes[i].left = nodes[left_index]
+        if right_index < len(nodes):
+            nodes[i].right = nodes[right_index]
+    
+    return nodes[0]  # Повертаємо кореневий вузол купи у вигляді дерева
+
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_heap_as_tree(heap):
+    tree_root = build_heap_tree(heap)
+    if not tree_root:
+        return
+    
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+    
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використання значень вузлів як міток
+    
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+# Створення та візуалізація бінарної купи у вигляді дерева
+heap = [3, 1, 6, 5, 2, 8, 10]
+heapq.heapify(heap)
+draw_heap_as_tree(heap)
